52d32f3384aceae412f4310659420ebd6fb3a1b3
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-14 17:53:59 +0300

    Baslık Foto

--- NavControllerItem/ViewController.swift
+++ NavControllerItem/ViewController.swift
@@ -12,7 +12,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let image = UIImage(named: "navBaslik")!
+        
+        self.navigationItem.titleView = UIImageView(image: image)
+        
+        
     }
+    
+    
 
 
 }
