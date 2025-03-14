93b1d29c9c374101126ec1eb10790934dbd9a213
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-14 17:28:29 +0300

    Initial Commit

---
+++ NavControllerItem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavControllerItem
+//
+//  Created by Ömer on 14.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NavControllerItemTests/NavControllerItemTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavControllerItemTests.swift
+//  NavControllerItemTests
+//
+//  Created by Ömer on 14.03.2025.
+//
+
+import Testing
+@testable import NavControllerItem
+
+struct NavControllerItemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
