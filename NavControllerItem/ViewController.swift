7f3cc277e64b42e9b1bd3993c66a45f3ab5012f5
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-14 17:59:49 +0300

    Segment

--- NavControllerItem/ViewController.swift
+++ NavControllerItem/ViewController.swift
@@ -13,15 +13,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let image = UIImage(named: "navBaslik")!
+       // let image = UIImage(named: "navBaslik")!
         
-        self.navigationItem.titleView = UIImageView(image: image)
+       // self.navigationItem.titleView = UIImageView(image: image)
         
         
     }
     
     
-
+    @IBAction func segmentTik(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("1")
+        case 1:
+            print("2")
+        default:
+            print("3")
+        }
+        
+        
+        
+    }
+    
 
 }
 
